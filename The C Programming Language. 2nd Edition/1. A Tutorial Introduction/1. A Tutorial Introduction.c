e653b2d9891118fafb233edbe4663e42b236fd28
kolos <Ed Kolosovsky>
2019-07-29 07:32:08 +0300

    C

---
+++ The C Programming Language. 2nd Edition/1. A Tutorial Introduction/1. A Tutorial Introduction.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/*main() {
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	celsius = lower;
+
+	printf("fahr  celsius\n");
+
+	while (celsius <= upper) {
+		fahr = celsius * 9.0 / 5.0 + 32;
+		printf("%4.0f  %7.1f\n", fahr, celsius);
+		celsius = celsius + step;
+	}
+}*/
+
+/*int main() {
+	int fahr;
+
+	for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	}
+
+	return 0;
+}*/
+
+int main() {
+	int c, i, nwhite, nother;
+	int ndigit[ 10];
+	nwhite = nother = 0;
+
+	for (i = 0; i < 10; ++i) {
+		ndigit[i] = 0;
+	}
+
+
+
+	return 0;
+}
+
+
+
